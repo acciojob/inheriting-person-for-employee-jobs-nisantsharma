b7ef1490e6a8734ecdaa552171a01062ecbe9d7f
NISANT SHARMA <106690246+nisantsharma@users.noreply.github.com>
2024-08-13 20:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-// complete this js code
-function Person(name, age) {}
+// Person constructor function
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Method added to the Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Employee constructor function
+function Employee(name, age, jobTitle) {
+  // Inherit properties from Person
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee; // Ensure the constructor is correct
+
+// Method added to the Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
 
 // Do not change code below this line
 window.Person = Person;
